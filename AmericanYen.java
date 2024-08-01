2e7d6c02aa6e13641c818400650fd08c742508d7
Derricko Swink <dswinkgfx@gmail.com>
2024-07-31 21:27:29 -0400

    Add files via upload

---
+++ AmericanYen.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class AmericanYen {
+
+    public static void space(){
+        System.out.println();
+    }
+
+    public static void AmericanYen(){
+        System.out.println("---------$--------");
+        System.out.println("$  American Yen  $");
+        System.out.println("---------$--------");
+    }
+
+    // main
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        double usdAmount = 0.0;
+        double yen = 149.02;
+
+        AmericanYen();
+        space();
+
+        while (!(usdAmount == -1)) {
+            usdAmount++;
+            System.out.print("Press -1 to quit.");
+            System.out.print("\nEnter US dollar amount for Japanese Yen conversion: $");
+            usdAmount = scan.nextDouble();
+
+            double convRate = usdAmount * yen;
+
+            System.out.println("---------$--------");
+            System.out.printf("American Dollar: $%.2f", usdAmount);
+            System.out.println("\nJapanese Yen Dollar: ¥" + df.format(convRate));
+            System.out.println("---------$--------");
+
+        } if (usdAmount == -1) {
+            System.out.print("You've exited the program.");
+            scan.close();
+        }
+    }
+}
